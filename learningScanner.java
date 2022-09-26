3407452fabdc1cfc3d23227241c9b7e0ec92c357
emilioharaldsson <stephan.haraldsson@gmail.com>
2022-09-26 13:16:07 -0400

    branch commit

--- learningScanner.java
+++ learningScanner.java
@@ -2,5 +2,12 @@ public class learningScanner {
     public static void main(String[] args) {
         // Scanner is a way to make apps dynamic and interactive
         // Scanner 'scans' user input into 
+        int myArray[] = {1,2,3,4,5,6,7,8,9,10};
+        int count = 0; 
+        for (int i = 0; i <= myArray.length; i++ ) {
+            count += 1;
+        }
+        System.out.println(count);
+        
     }
 }
