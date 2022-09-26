0bbaca897fde0fcf93be9bb680a0b99b4f64c938
emilioharaldsson <stephan.haraldsson@gmail.com>
2022-09-26 11:17:20 -0400

    second testy test :)

--- GitActivity.java
+++ GitActivity.java
@@ -1,5 +1,6 @@
 public class GitActivity {
     public static void main(String[] args) {
-
+        int numberOfFans = 5000;
+        System.out.println("There are "+numberOfFans+ " fans at my show");
     }
 }
